e7c70ad29c7f2cbce0c1fc3c5d58b5b1d313ca30
thomassdk <thomassdk@pm.me>
2022-05-20 16:42:39 +0200

    Init eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'plugin:storybook/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte']
+  },
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.*', 'vite.config.*', 'svelte.config.*'],
+  overrides: [{
+    files: ['*.svelte'],
+    processor: 'svelte3/svelte3'
+  }],
+  settings: {
+    'svelte3/typescript': () => require('typescript') // pass the TypeScript package to the Svelte plugin
+  }
+};
